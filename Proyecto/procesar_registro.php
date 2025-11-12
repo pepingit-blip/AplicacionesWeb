86cb9b084a15b8fe87672ca6e5b1abeb3d2928f9
nicolas.swiatek <pepingit@gmail.com>
2025-11-12 14:09:44 -0500

    Final

--- Proyecto/procesar_registro.php
+++ Proyecto/procesar_registro.php
@@ -5,6 +5,19 @@ $password = $_POST['password'];
 $file = fopen("usuarios.txt", "a");
 fwrite($file, $usuario . ":" . password_hash($password, PASSWORD_DEFAULT) . "\n");
 fclose($file);
-echo "<h1>Usuario registrado correctamente</h1>";
-echo "<p><a href='login.php'>Iniciar sesión</a></p>";
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro_correcto</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <h2>Usuario registrado correctamente</h2>
+    <p style="margin-top: 20px;">
+    <a href='login.php'>Iniciar sesión</a>
+</p>
+</body>
+</html>
