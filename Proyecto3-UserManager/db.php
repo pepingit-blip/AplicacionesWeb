719fce8a68dc2b7b5c4719cdf2872dba98affedd
nicolas.swiatek <pepingit@gmail.com>
2025-12-10 14:25:35 -0500

    Principio

---
+++ Proyecto3-UserManager/db.php
@@ -0,0 +1,13 @@
+<?php
+$host="localhost";
+$dbname="usermanager";
+$user="root";
+$pass="";
+
+try {
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+die("Error de conexión: " . $e->getMessage());
+}
+?>
