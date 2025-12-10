63498cbd476ff8307a20cd24414205bbc6631b93
nicolas.swiatek <pepingit@gmail.com>
2025-12-10 14:59:26 -0500

    Principiov2

---
+++ Proyecto3-UserManager/index.php
@@ -0,0 +1,15 @@
+<?php include "db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UFT-8">
+        <title>UserManager</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="Contenedor">
+            <h1>Gestión de Usuarios</h1>
+            <a class="btn" href="list.php">Ir al CRUD</a>
+        </div>
+    </body>
+</html>
