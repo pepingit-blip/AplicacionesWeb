6fb3aea7d1eccf4944a36647f6a99e9b4e35f06a
nicolas.swiatek <pepingit@gmail.com>
2025-12-12 10:20:01 -0500

    Base v1

---
+++ Proyecto3-UserManager/create.php
@@ -0,0 +1,39 @@
+<?php 
+include"db.php";
+
+if ($_POST){
+    $nombre=$_POST["nombre"];
+    $email=$_POST["email"];
+    $edad=$_POST["edad"];
+    $rol=$_POST["rol"];
+
+    $stmt = $pdo->prepare("INSERT INTO usuarios (nombre,eail,edad,rol)VALUES(?,?,?,?)");
+    $stmt->execute([$nombre,$email,$edad,$rol]);
+
+    header("Location:list.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Crear Usuario</title>
+        <link rel="stylesheet" href="css/Styles.css">
+    </head>
+    <body>
+        <div class="contenedor-forma">
+            <h1>Crear Usuario</h1>
+            <form method="POST">
+            <input type="text" name="nombre" placeholder="Nombre" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="number" name="edad" placeholder="Edad" required>
+            <select name="rol">
+                <option value="user">Usuario</option>
+                <option value="admin">Administrador</option>
+            </select>
+            <button class="btn" type="submit">Guardar</button>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ Proyecto3-UserManager/edit.php
@@ -0,0 +1,42 @@
+<?php
+include "db.php";
+$id = $_GET["id"];
+$stmt = $pdo->prepare("SELECT * FROM usuarios WHERE id=?");
+$stmt->execute([$id]);
+$usuario = $stmt->fetch();
+if ($_POST) {
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$edad = $_POST["edad"];
+$rol = $_POST["rol"];
+
+$update = $pdo->prepare("UPDATE usuarios SET nombre=?,email=?,rol=? WHERE id=?");
+$update->execute([$nombre,$email,$edad,$rol,$id]);
+
+header("Location:list.php");
+exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Editar Usuario</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="form-container">
+            <h1>Editar Usuario</h1>
+            <form method="POST">
+            <input type="text" name="nombre" value="<?= $usuario['nombre'] ?>" required>
+            <input type="email" name="email" value="<?= $usuario['email'] ?>" required>
+            <input type="number" name="edad" value="<?= $usuario['edad'] ?>" required>
+            <select name="rol">
+            <option value="user" <?= $usuario['rol']=='user'?'selected':'' ?>>Usuario</option>
+            <option value="admin" <?= $usuario['rol']=='admin'?'selected':'' ?>>Administrador</option>
+            </select>
+            <button class="btn" type="submit">Actualizar</button>
+            </form>
+        </div>
+    </body>
+</html>
