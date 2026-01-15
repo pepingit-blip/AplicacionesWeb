c793ad66c9374f438416c2788f1b7a44b600dfef
nicolas.swiatek <pepingit@gmail.com>
2026-01-15 13:49:02 -0500

    Amen2.0

--- Proyecto3-UserManager/admin/delete.php
+++ Proyecto3-UserManager/admin/delete.php
@@ -1,5 +1,5 @@
 <?php
-include "includes/db.php";
+require_once __DIR__ . '/../includes/db.php';
 
 $id = $_GET["id"];
 $stmt = $conn->prepare("DELETE FROM usuarios WHERE id=?");
--- Proyecto3-UserManager/admin/procesar_edit.php
+++ Proyecto3-UserManager/admin/procesar_edit.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "includes/db.php";
+require_once __DIR__ . '/../includes/db.php';
 
 $id = $_POST["id"];
 $nombre = $_POST["nombre"];
